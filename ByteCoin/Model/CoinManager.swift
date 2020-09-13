746817d6424996ad40a645f8d3103f164d1a0cbb
Anthony Liu <liu_anthony@apple.com>
2020-09-12 19:51:52 -0700

    Read key from local coinApiKey file

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,26 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    var apiKey: String?
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    init() {
+        
+        // Read the API key from the coinApiKey.txt file
+        if let keyPath = Bundle.main.path(forResource: "coinApiKey", ofType:"txt") {
+            
+            do {
+                let fileContents = try String(contentsOfFile: keyPath, encoding: .utf8)
+                self.apiKey = fileContents.trimmingCharacters(in: .whitespacesAndNewlines)
+            } catch {
+                print(error)
+            }
+        }
+        print(self.apiKey!)
+    }
+    
+    func getCoinPrice(currency: String) -> String {
+        return currency
+    }
 }
